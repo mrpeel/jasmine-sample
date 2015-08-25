9d4dced1714fd091981e372a045dbb8e12ce2db5
mrpeel <mrpeel@gmail.com>
2015-08-25 14:43:24 +1000

    Adds promise support

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,44 +1,74 @@
-describe('Address Book', function() {
+describe('Address Book', function () {
     var addressBook, thisContact;
-    
-    beforeEach(function() {
+
+    beforeEach(function () {
         addressBook = new AddressBook();
         thisContact = new Contact();
     });
-    
-    it('should be able to add a contact', function() {
-        
-        
+
+    it('should be able to add a contact', function () {
+
+
         addressBook.addContact(thisContact);
-        
-        expect(addressBook.getContact(0)).toBe(thisContact);        
+
+        expect(addressBook.getContact(0)).toBe(thisContact);
     });
-    
-    it('should be able to delete a contact', function() {
-        var addressBook = new AddressBook(), thisContact = new Contact();
-        
+
+    it('should be able to delete a contact', function () {
+        var addressBook = new AddressBook(),
+            thisContact = new Contact();
+
         addressBook.addContact(thisContact);
         addressBook.deleteContact(0);
-        
+
         expect(addressBook.getContact(0)).not.toBeDefined();
-        
+
     });
-    
+
 });
 
-describe('Async Address Book', function() {
+describe('Async Address Book', function () {
     var addressBook = new AddressBook();
-    
-    beforeEach(function(done) {
-        addressBook.getInitialContacts(function() {
+
+    beforeEach(function (done) {
+        addressBook.getInitialContacts(function () {
             done();
         });
     });
-    
-    it('should grab initial contacts', function(done) {
+
+    it('should grab initial contacts', function (done) {
         expect(addressBook.initialComplete).toBe(true);
         done();
     });
-    
-    
+
+
+});
+
+describe('Promise Address Book', function () {
+    var addressBook = new AddressBook();
+
+    beforeEach(JasminePromiseMatchers.install);
+
+
+    it('should resolve', function (done) {
+        expect(addressBook.promiseMe()).toBeResolved(done);
+
+    });
+
+    it('should return result', function (done) {
+        expect(addressBook.promiseMe()).toBeResolvedWith('result', done);
+
+    });
+
+    it('should reject', function (done) {
+        expect(addressBook.rejectMe()).toBeRejected(done);
+    });
+
+    it('should reject with error', function (done) {
+        expect(addressBook.rejectMe()).toBeRejectedWith('error', done);
+    });
+
+
+
+    afterEach(JasminePromiseMatchers.uninstall);
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,29 +1,45 @@
 function AddressBook() {
     this.contacts = [];
     this.initialComplete = false;
-    
+
 }
 
-AddressBook.prototype.getInitialContacts = function(cb) {
+AddressBook.prototype.getInitialContacts = function (cb) {
     var self = this;
-    
-    setTimeout(function() {
+
+    setTimeout(function () {
         self.initialComplete = true;
-        if(cb) {
+        if (cb) {
             return cb();
         }
-    },3);
-        
+    }, 3);
+
 }
 
-AddressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function (contact) {
     this.contacts.push(contact);
 }
 
-AddressBook.prototype.getContact = function(index) {
+AddressBook.prototype.getContact = function (index) {
     return this.contacts[index];
 }
 
-AddressBook.prototype.deleteContact = function(index) {
-    this.contacts.splice(index,1);
+AddressBook.prototype.deleteContact = function (index) {
+    this.contacts.splice(index, 1);
+}
+
+AddressBook.prototype.promiseMe = function () {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve("result");
+        }, 500);
+    });
+}
+
+AddressBook.prototype.rejectMe = function () {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            reject("error");
+        }, 500);
+    });
 }
